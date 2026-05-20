41b2858a9444ad95a79d18e4994325767f79e46c
victoria <pururpu@gmail.com>
2026-05-20 22:37:29 +0700

    Fix Program model

--- app/models/program.py
+++ app/models/program.py
@@ -8,5 +8,5 @@ class Program(Base):
     __tablename__ = "programs"
 
     id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, autoincrement=True, default=uuid.uuid4)
-    program_name: Mapped[str] = mapped_column(String(255), nullable=False)
     user_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
+    program_name: Mapped[str] = mapped_column(String(255), nullable=False)
