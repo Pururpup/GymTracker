ff70a0cc83d06ad9d2f0539f8e3170f5c9f22b1f
victoria <pururpu@gmail.com>
2026-05-20 21:58:19 +0700

    Added Program model

---
+++ app/models/program.py
@@ -0,0 +1,12 @@
+import uuid
+from sqlalchemy import String, UUID, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+from models.base import Base
+
+
+class Program(Base):
+    __tablename__ = "programs"
+
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, autoincrement=True, default=uuid.uuid4)
+    program_name: Mapped[str] = mapped_column(String(255), nullable=False)
+    user_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
